bc9b50799640a7e8348a4249985bfb3e31648777
Matheus Roika <matheusroika@hotmail.com>
2021-05-13 09:08:46 -0300

    Configurando MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de website',
+          amount: 12000,
+          type: 'income',
+          category: 'Desenvolvimento',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
