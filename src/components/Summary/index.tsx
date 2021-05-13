883f44c19438c355856ccd3d4816614bae881ba5
Matheus Roika <matheusroika@hotmail.com>
2021-05-13 07:57:07 -0300

    Componente: Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <span>R$ <strong>1.000</strong>,00</span>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <span>R$ <strong>500</strong>,00</span>
+      </div>
+
+      <div>
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <span>R$ <strong>500</strong>,00</span>
+      </div>
+    </Container>
+  )
+}
